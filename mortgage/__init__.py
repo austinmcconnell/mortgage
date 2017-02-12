b425016c86dfb36d79b85a77d7961c0fd7966380
Austin McConnell <austin.s.mcconnell@gmail.com>
2017-02-11 21:26:43 -0600

    Changed version back to 0.1.0 in order to use bumpversion

--- mortgage/__init__.py
+++ mortgage/__init__.py
@@ -1,1 +1,5 @@
+__author__ = 'Austin McConnell'
+__email__ = 'austin.s.mcconnell@gmail.com'
+__version__ = '0.1.0'
+
 from mortgage.loan import Loan
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ test_requirements = [
 
 setup(
     name='mortgage',
-    version='1.0.0',
+    version='0.1.0',
     description='Mortgage Calculator',
     long_description=readme,
     author='Austin McConnell',
