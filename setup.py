f29431b6b663e282bb52a6c4ad224752e1f5eecf
Austin McConnell <austin.s.mcconnell@gmail.com>
2017-07-30 14:33:20 -0500

    Fixed pylint errors

--- setup.py
+++ setup.py
@@ -1,28 +1,21 @@
 from setuptools import setup, find_packages
 
 with open('README.md') as readme_file:
-    readme = readme_file.read()
+    README = readme_file.read()
 
-requirements = [
-    'python-dateutil >=2.5.0, < 3.0'
-]
-
-test_requirements = [
-    'pytest >=3.0.5, <4.0',
-    'coverage >=4.3, <5.0',
-    'bumpversion >=0.5.3, < 1.0'
-]
+with open('requirements.txt') as file:
+    REQUIREMENTS = file.read()
 
 setup(
     name='mortgage',
     version='1.0.0',
     description='Mortgage Calculator',
-    long_description=readme,
+    long_description=README,
     author='Austin McConnell',
     author_email='austin.s.mcconnell@gmail.com',
     url='https://github.com/austinmcconnell/mortgage',
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=REQUIREMENTS,
     license='MIT license',
     zip_safe=False,
     keywords='mortgage',
@@ -36,6 +29,5 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
-    test_suite='tests',
-    tests_require=test_requirements
+    test_suite='tests'
 )
