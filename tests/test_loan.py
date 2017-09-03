a98bd9b92534036cbe33b8359f23a644700c3bb4
Austin McConnell <austin.s.mcconnell@gmail.com>
2017-09-02 20:16:27 -0500

    Convert to using PyTest fixtures

--- tests/test_loan.py
+++ tests/test_loan.py
@@ -7,12 +7,16 @@ from mortgage import Loan
 def convert(value):
     return Decimal(value).quantize(Decimal('0.01'))
 
-class TestLoan:
 
-    loan = Loan(principal=200000, interest=.06, term=30, start_date=(2017, 3, 1))
+@pytest.fixture(scope='class')
+def loan_200k():
+    return Loan(principal=200000, interest=.06, term=30, start_date=(2017, 3, 1))
 
-    def test_monthly_payment(self):
-        assert convert(self.loan.monthly_payment) == convert(1199.10)
+
+class TestLoan(object):
+
+    def test_monthly_payment(self, loan_200k):
+        assert convert(loan_200k.monthly_payment) == convert(1199.10)
 
     nth_principal = [
         (1, 199.10),
@@ -22,8 +26,8 @@ class TestLoan:
     ]
 
     @pytest.mark.parametrize('nth_payment, principal', nth_principal)
-    def test_nth_principal_payment(self, nth_payment, principal):
-        assert convert(self.loan.schedule(nth_payment)['principal']) == convert(principal)
+    def test_nth_principal_payment(self, loan_200k, nth_payment, principal):
+        assert convert(loan_200k.schedule(nth_payment)['principal']) == convert(principal)
 
     nth_interest = [
         (1, 1000.00),
@@ -33,8 +37,8 @@ class TestLoan:
     ]
 
     @pytest.mark.parametrize('nth_payment, interest', nth_interest)
-    def test_nth_interest_payment(self, nth_payment, interest):
-        assert convert(self.loan.schedule(nth_payment)['interest']) == convert(interest)
+    def test_nth_interest_payment(self, loan_200k, nth_payment, interest):
+        assert convert(loan_200k.schedule(nth_payment)['interest']) == convert(interest)
 
     nth_total_interest = [
         (1, 1000.00),
@@ -44,8 +48,8 @@ class TestLoan:
     ]
 
     @pytest.mark.parametrize('nth_payment, total_interest', nth_total_interest)
-    def test_nth_total_interest_payment(self, nth_payment, total_interest):
-        assert convert(self.loan.schedule(nth_payment)['total_interest']) == convert(total_interest)
+    def test_nth_total_interest_payment(self, loan_200k, nth_payment, total_interest):
+        assert convert(loan_200k.schedule(nth_payment)['total_interest']) == convert(total_interest)
 
     nth_balance = [
         (1, 199800.90),
@@ -55,35 +59,35 @@ class TestLoan:
     ]
 
     @pytest.mark.parametrize('nth_payment, balance', nth_balance)
-    def test_nth_balance(self, nth_payment, balance):
-        assert convert(self.loan.schedule(nth_payment)['balance']) == convert(balance)
+    def test_nth_balance(self, loan_200k, nth_payment, balance):
+        assert convert(loan_200k.schedule(nth_payment)['balance']) == convert(balance)
 
-    def test_original_balance(self):
-        assert self.loan.principal == convert(200000.00)
+    def test_original_balance(self, loan_200k):
+        assert loan_200k.principal == convert(200000.00)
 
-    def test_interest_rate(self):
-        assert self.loan.interest == convert(.06)
+    def test_interest_rate(self, loan_200k):
+        assert loan_200k.interest == convert(.06)
 
-    def test_apy(self):
-        assert self.loan.apy == convert(6.17)
+    def test_apy(self, loan_200k):
+        assert loan_200k.apy == convert(6.17)
 
-    def test_apr(self):
-        assert self.loan.apr == convert(6.00)
+    def test_apr(self, loan_200k):
+        assert loan_200k.apr == convert(6.00)
 
-    def test_term(self):
-        assert self.loan.term == 30
+    def test_term(self, loan_200k):
+        assert loan_200k.term == 30
 
-    def test_total_principal(self):
-        assert self.loan.total_principal == convert(200000.00)
+    def test_total_principal(self, loan_200k):
+        assert loan_200k.total_principal == convert(200000.00)
 
-    def test_total_interest(self):
-        assert self.loan.total_interest == convert(231676.38)
+    def test_total_interest(self, loan_200k):
+        assert loan_200k.total_interest == convert(231676.38)
 
-    def test_total_paid(self):
-        assert self.loan.total_paid == convert(431676.38)
+    def test_total_paid(self, loan_200k):
+        assert loan_200k.total_paid == convert(431676.38)
 
-    def test_interest_to_principle(self):
-        assert self.loan.interest_to_principle == convert(115.8)
+    def test_interest_to_principle(self, loan_200k):
+        assert loan_200k.interest_to_principle == convert(115.8)
 
-    def test_years_to_pay(self):
-        assert self.loan.years_to_pay == 30
+    def test_years_to_pay(self, loan_200k):
+        assert loan_200k.years_to_pay == 30
