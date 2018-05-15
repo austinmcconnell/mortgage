2ce7d732ec4f1ee2f14be6e69f0495a7a873a9b4
Austin McConnell <austin.s.mcconnell@gmail.com>
2018-05-15 13:48:00 -0500

    Added recommonmark to requirements

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
         'develop': ['bumpversion>=0.5.3,<1.0.0',
                     'pytest>=3.2.1,<4.0.0',
                     'pytest-cov>=2.5.1,<3.0.0',
+                    'recommonmark>=0.4.0,<1.0.0',
                     'sphinx>=1.7.4,<2.0.0',
                     'sphinx-autodoc-typehints>=1.3.0,<2.0.0',
                     'twine>=1.11.0,<2.0.0',]
