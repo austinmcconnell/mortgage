5f04a98548d287a23d440f448b0cd974f2973605
Austin McConnell <austin.s.mcconnell@gmail.com>
2019-05-05 11:14:10 -0400

    Bump version: 1.0.2 → 1.0.3rc

--- mortgage/__init__.py
+++ mortgage/__init__.py
@@ -15,6 +15,6 @@
 
 __author__ = 'Austin McConnell'
 __email__ = 'austin.s.mcconnell@gmail.com'
-__version__ = '1.0.2'
+__version__ = '1.0.3rc'
 
 from mortgage.loan import Loan
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='mortgage',
-    version='1.0.2',
+    version='1.0.3rc',
     description='Mortgage Calculator',
     long_description=long_description,
     long_description_content_type='text/markdown',
